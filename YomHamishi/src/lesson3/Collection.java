ffa28c365981e708b838ddcf5cfebc82208e263c
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-11-10 09:12:51 +0200

    fix TestFiles

--- YomHamishi/src/lesson3/Collection.java
+++
@@ -1,8 +0,0 @@
-package lesson3;
-
-public interface Collection<T> {
-	void add(T item);
-	//void remove(T item);
-	//boolean isEmpty();
-	boolean contains(T item);
-}
--- YomHamishi/src/lesson3/List.java
+++
@@ -1,6 +0,0 @@
-package lesson3;
-
-public interface List<T> extends Collection<T> {
-	T get(int index);
-	void put(T item, int index);
-}
