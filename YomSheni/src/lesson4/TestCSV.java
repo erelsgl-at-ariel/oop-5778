d6989adcc1610b884b5d47f8958529d9149fa001
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-11-15 19:39:36 +0200

    Add TestCSV

---
+++ YomSheni/src/lesson4/TestCSV.java
@@ -0,0 +1,23 @@
+package lesson4;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+public class TestCSV {
+
+	public static void main(String[] args) throws IOException {
+		Reader in = new FileReader("test.csv");
+		Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(in);
+		for (CSVRecord record : records) {
+		    String name = record.get("Name");
+		    String phone = record.get("Phone");
+		    System.out.println(name+" "+phone);
+		}	
+	}
+
+}
