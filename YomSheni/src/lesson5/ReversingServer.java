e293022270de05069ded54798604a2269b819ea7
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-11-15 20:38:56 +0200

    Serve the HTML files from the same server

--- YomSheni/src/lesson5/ReversingServer.java
+++ YomSheni/src/lesson5/ReversingServer.java
@@ -1,14 +1,11 @@
 package lesson5;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 /**
