94eae61a8833dba923a5d5f0d7028d29de214fb2
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-11-30 07:33:50 +0200

    Fix bug

--- YomSheni/src/lesson6/ConcurrencyTestExecutors3.java
+++ YomSheni/src/lesson6/ConcurrencyTestExecutors3.java
@@ -13,25 +13,25 @@ import java.util.concurrent.*;
  */
 public class ConcurrencyTestExecutors3 {
 
-	static final int SIZE = 10000000;
-	static final int NUM_OF_THREADS = 4;
+	static final int SIZE = 2*3*4*5*6*7*8*9*10;
+	static final int NUM_OF_THREADS = 1;
+	static final int SIZE_PER_THREAD = SIZE/NUM_OF_THREADS;
 
-	static int iThread;
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		int[] array = new int[NUM_OF_THREADS*SIZE];
+		int[] array = new int[SIZE];
 		Arrays.fill(array, 1);
 		
 		ExecutorService executor = Executors.newCachedThreadPool();
 		Instant startTime = Instant.now();
 
 		CompletableFuture<Integer>[] futures = new CompletableFuture[NUM_OF_THREADS];
-		for (iThread=0; iThread<NUM_OF_THREADS; ++iThread) {
+		for (int iThread=0; iThread<NUM_OF_THREADS; ++iThread) {
 			final int iThreadLocal = iThread;
 			futures[iThread] = CompletableFuture.supplyAsync( () -> {
 				int sum=0;
-				int start = iThreadLocal*SIZE;
-				int end = (iThreadLocal+1)*SIZE;
+				int start = iThreadLocal*SIZE_PER_THREAD;
+				int end = (iThreadLocal+1)*SIZE_PER_THREAD;
 				for (int i=start; i<end; ++i) {
 					Thread.yield();
 					sum += Math.pow(Math.pow(Math.pow(array[i],0.3),0.4),0.5);
@@ -47,7 +47,7 @@ public class ConcurrencyTestExecutors3 {
 					try {
 						Arrays.stream(new int[] {1,2,3}).sum();
 						int totalSum = 0;
-						for (iThread=0; iThread<NUM_OF_THREADS; ++iThread)
+						for (int iThread=0; iThread<NUM_OF_THREADS; ++iThread)
 							totalSum += futures[iThread].get();
 						System.out.println("Total: "+totalSum+ 
 								"   "+Duration.between(startTime, Instant.now()).toMillis()+" [ms]");
