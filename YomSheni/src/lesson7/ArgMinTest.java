f44b287411b2add52a310a6fca0623ada1f93a2d
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-12-01 05:21:09 +0200

    Add arg-min test

---
+++ YomSheni/src/lesson7/ArgMinTest.java
@@ -0,0 +1,27 @@
+package lesson7;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+
+/**
+ * An example of finding the arg-min of a function using streams.
+ * 
+ * @author erelsgl
+ */
+public class ArgMinTest {
+	static double f(int i) {
+		return Math.sin(i/100.0);
+	}
+	
+	public static void main(String[] args) {
+		Comparator<Integer> sineComparator = (i,j) -> 
+			Double.compare(f(i), f(j));
+		int bestIndex = IntStream.range(1,1000)
+		.mapToObj(i->i)
+		.min(sineComparator)
+		.get();
+		System.out.println(bestIndex+ " " + f(bestIndex));
+	}
+
+}
