9d5b927924d77506e2021cd62526330d83dd6d8d
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-12-25 19:46:44 +0200

    add eclipse-gradle project

---
+++ week10-gradle/25-eclipse/src/main/java/lesson10/Hello.java
@@ -0,0 +1,11 @@
+package lesson10;
+
+import static spark.Spark.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        port(8080);
+        get("/hello", (req, res) -> "Hello World");
+        get("/bye", (req, res) -> "Goodbye World");
+    }
+}
