3756e5faab416777be1d80cf9812ac71a80b1da3
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-12-26 09:06:10 +0200

    Deploying

--- week10-gradle/25-tricks/deploy.sh
+++ week10-gradle/25-tricks/deploy.sh
@@ -1,1 +1,8 @@
-scp build/libs/*.jar root@162.243.116.217:~/jars/
+echo -- add --
+/usr/bin/git add -A
+echo -- commit --
+/usr/bin/git commit -m "Deploying"
+echo -- pull --
+/usr/bin/git pull
+echo -- push --
+/usr/bin/git push
