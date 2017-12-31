75ff04fd6d9128b7b7401a722d1c8ad29472331a
Erel Segal-Halevi cron <erelsgl@gmail.com>
2017-12-31 15:15:44 +0200

    Add MySQL explanation

--- week11-databases/src/main/java/db/CompanyDatabase.java
+++ week11-databases/src/main/java/db/CompanyDatabase.java
@@ -6,20 +6,20 @@ public class CompanyDatabase {
 	
 	/**
 	 * Connect to a local SQLite database
-	 *
+	 */
 	static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection("jdbc:sqlite:company.db");
-	}*/
+	}
 	
 	
 	/**
 	 * Connect to a remote MySQL database
 	 */
-	static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(
-			/* database= */ "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11213398", 
-			/* username= */ "sql11213398",
-			/* password= */ "jNlyMq3Kdd");
-	}
+//	static Connection getConnection() throws SQLException {
+//		return DriverManager.getConnection(
+//			/* database= */ "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11213398", 
+//			/* username= */ "sql11213398",
+//			/* password= */ "jNlyMq3Kdd");
+//	}
 	
 }
