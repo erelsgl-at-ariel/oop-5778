2e3f59b20cac7e6e90cf323f386434128438383c
Erel Segal-Halevi cron <erelsgl@gmail.com>
2018-01-01 16:26:04 +0200

    week03 code

---
+++ week03-collections/src/yom2/TestException2.java
@@ -0,0 +1,46 @@
+package yom2;
+
+import java.util.logging.*;
+import java.io.*;
+
+public class TestException2 {
+	static void p(String s) {System.out.println(s);}
+	
+	public static void main(String[] args) { 
+		p("main1"); 
+		a(); 
+		p("main2"); 
+	}
+	
+	static void a() {
+		p("a1");
+		try {
+			b();
+		} catch (NullPointerException ex) {
+			System.out.println("Did you use a null object? "+ex);
+		} catch (ArithmeticException ex) {
+			System.out.println("Did you divide by zero? "+ex);
+		} catch (RuntimeException ex) {
+//			System.out.println("What did you do? "+ex);
+//			Logger.getGlobal().severe(ex.toString());
+			StringWriter w = new StringWriter();
+			ex.printStackTrace(new PrintWriter(w));
+			Logger.getGlobal().severe(w.toString());
+		}
+		p("a2");
+	}
+	
+	static void b() {
+		p("b1"); 
+		c();	
+		p("b2"); 
+	}
+	
+	static void c() {
+		Object x = null;
+//		String s = x.toString();
+		//int i = 5/0;
+//		throw new IllegalArgumentException("hah!");
+		throw new IllegalColorException("abc");
+	}
+}
