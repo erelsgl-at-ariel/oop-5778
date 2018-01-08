1367b77900a49767a7ec3a177af4485d13ceb1cf
Student <Student@C0934-00>
2018-01-08 13:11:15 +0200

    Add test to prototype

--- week12-designpatterns/src/prototype/PrototypeDemo.java
+++ week12-designpatterns/src/prototype/PrototypeDemo.java
@@ -1,5 +1,7 @@
 package prototype;
 
+import java.util.ArrayList;
+
 public class PrototypeDemo {
 	static void plotShapeFamily(Shape prototype, int fromSize, int toSize) {
 		for (int size=fromSize; size<toSize; ++size) {
@@ -9,8 +11,28 @@ public class PrototypeDemo {
 		}
 	}
 	
-	public static void main(String[] args) {
+	static void simpleTest() {
 		plotShapeFamily(new Square(5,"blue"), 10,20);
 		plotShapeFamily(new Triangle(6,"green"), 10,20);
+	}
+	
+	static void complexTest() {
+		SquareList prototype = new SquareList(new ArrayList<Square>());
+		prototype.addSquare(new Square(5,"green"));
+		prototype.plot();
+		System.out.println();
+		
+		SquareList c = (SquareList)prototype.clone();
+		c.addSquare(new Square(10,"green"));
+		c.getSquares().get(0).setSize(100);
+		c.plot();
+		System.out.println();
+		
+		prototype.plot();
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		simpleTest();
 	}
 }
