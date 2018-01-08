ac159310953874ef0987e73c7f9ea7f23c8b568f
Student <Student@C0934-00>
2018-01-08 13:04:19 +0200

    adapted demo

--- week12-designpatterns/src/adapter/AdapterDemo.java
+++ week12-designpatterns/src/adapter/AdapterDemo.java
@@ -12,6 +12,10 @@ public class AdapterDemo {
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1,2,3);
 		Iterator<Integer> iterator = list.iterator();
+		
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
 		print(iterator);
 	}
 }
